8793857b09f3b2f91d142cc5abd730b24f6ae3a7
Vigneshwaran B <vigneshsobalamurugan2005@gmail.com>
2026-04-09 23:37:51 +0530

    Updated with User Agents

--- Task1/app.py
+++ Task1/app.py
@@ -2,21 +2,45 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
 import time
 import pandas as pd
 from datetime import date
 from database import create_table, insert_products
+import random
 
-def scrape_products():
-    today = str(date.today())
+USER_AGENTS = [
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/122.0 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 13_0) AppleWebKit/537.36 Chrome/120.0 Safari/537.36",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 Chrome/118.0 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Firefox/123.0",
+    "Chrome/122.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Safari/537.36 Edg/122.0",
+    "Chrome/120.0 (Macintosh; Intel Mac OS X 13_0) AppleWebKit/537.36 Safari/537.36 Edg/120.0",
+    "Chrome/118.0 (X11; Linux x86_64) AppleWebKit/537.36 Safari/537.36 Edg/118.0",
+    "Chrome/122.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Safari/537.36 OPR/122.0",
+]
+
+def get_random_driver():
+
+    options = Options()
+    user_agent = random.choice(USER_AGENTS)
+    options.add_argument(f"user-agent={user_agent}")
+
+    options.add_argument("--disable-blink-features=AutomationControlled")
+    options.add_experimental_option("excludeSwitches",["enable-automation"])
+    options.add_experimental_option("useAutomationExtension",False)
 
     service = Service(ChromeDriverManager().install())
 
-    driver = webdriver.Chrome(service=service)
+    driver = webdriver.Chrome(service=service,options=options)
+    return driver
 
+def scrape_products():
+    today = str(date.today())
     data = []
     for page in range(1, 6):
             url = f"https://www.amazon.in/s?k=laptop&page={page}"
+            driver = get_random_driver()  
             driver.get(url)
             time.sleep(5)
             products = driver.find_elements(
@@ -47,8 +71,7 @@ def scrape_products():
                     link = "N/A"
 
                 data.append((name, price, today, link))
-
-    driver.quit()
+            driver.quit()
     return data
 
 create_table()
